fb892eb67427d8671e92cdbb951c91ec192986d4
Samay Samrat <samaysamrat@Samays-MacBook-Pro.local>
2026-01-01 19:03:34 +0530

    intial Restro Mern Project

---
+++ backend/app.js
@@ -0,0 +1,66 @@
+// import express from "express";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import { errorMiddleware } from "./middlewares/error.js";
+// import reservationRouter from "./routes/reservationRoute.js";
+// import { dbConnection } from "./database/dbConnection.js";
+
+// const app = express();
+// dotenv.config();
+
+
+// app.use(
+//   cors({
+//     origin: [process.env.FRONTEND_URL],
+//     methods: ["POST"],
+//     credentials: true,
+//   })
+// );
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use("/api/v1/reservation", reservationRouter);
+// app.get("/", (req, res, next)=>{return res.status(200).json({
+//   success: true,
+//   message: "HELLO WORLD AGAIN"
+// })})
+
+// dbConnection();
+
+// app.use(errorMiddleware);
+
+// export default app;
+
+
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { errorMiddleware } from "./middlewares/error.js";
+import reservationRouter from "./routes/reservationRoute.js";
+import { dbConnection } from "./database/dbConnection.js";
+
+dotenv.config();   // <-- FIX
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1/reservation", reservationRouter);
+
+app.get("/", (req, res) => {
+  res.status(200).json({ success: true, message: "Server running..." });
+});
+
+dbConnection();        // DB connect
+
+app.use(errorMiddleware);
+
+export default app;
